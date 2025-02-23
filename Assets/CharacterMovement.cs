6e1f474b1f5d2630685a303d07b98fe2f14dfdc2
arvinryan <arvinryanhf17@gmail.com>
2025-02-23 16:34:57 +0800

    Initial commit

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+
+    private CharacterController characterController; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * magnitude * Time.deltaTime, Space.World);
+        //characterController.SimpleMove(movementDirection * magnitude);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
